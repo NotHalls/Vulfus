bd38e00eef20bc7a6b1c026aa9e86751430a437e
NotHalls <notharshith74@gmail.com>
2024-09-12 03:53:47 +0530

    oops, commited some weird shit that is not needed and didnt commit the useful onces

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
+#include <GLFW/glfw3.h>
+#include <vulkan/vulkan.h>
+#include <glm.hpp>
+#include <imgui.h>
+
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello world";
+#ifdef VF_WINDOWS
+std::cout << "windows" << std::endl;
+#elif VF_UNIX
+std::cout << "unix" << std::endl;
+#endif
 }
